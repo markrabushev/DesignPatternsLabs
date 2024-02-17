7733807a57d6136ab95510823b334193824eb3ac
Mark Rabushev <100849266+markrabushev@users.noreply.github.com>
2024-02-17 14:19:15 +0300

    Add files via upload

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -1,42 +1,30 @@
 ﻿#include <iostream>
-#include "BusStation.h"
-#include "Board.h"
+
 using namespace std;
 
+class Singleton
+{
+private:
+    static Singleton* p_instance;
+
+    Singleton() {}
+    Singleton(const Singleton&);
+
+public:
+    static Singleton* getInstance() {
+        if (!p_instance)
+            p_instance = new Singleton();
+        return p_instance;
+    }
+};
+
+Singleton* Singleton::p_instance = nullptr;
 
 int main()
 {
-    BusStation* BS = new BusStation();
-    auto BT = BoardTaxi();
-    auto BB = BoardBus();
-    for (int i = 0; i < 180; i++)
-    {
-        BS->passengers.push_back(BT.BoardPassenger());
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        BS->drivers.push_back(BT.BoardDriver());
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        BS->drivers.push_back(BB.BoardDriver());
-    }
-    cout << BS->passengersCount() << endl;
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
 
-    for (int i = 0; i < BS->drivers.size(); i++)
-    {
-        if (BS->drivers[i]->licenze == Licenze::BCategory)
-        {
-            BS->passengers.erase(BS->passengers.begin(), BS->passengers.begin() + 4);
-        }
-        else
-        {
-            BS->passengers.erase(BS->passengers.begin(), BS->passengers.begin() + 30);
-        }
-    }
-
-    cout << BS->passengersCount() << endl;
-
-
+    cout << s1 << ' ' << s2 << endl;
 }
 
